9a80238729a2921f6327acdfe0970f674e91514b
Rhesa Prawedha <dr.prawedha@gmail.com>
2026-04-29 00:53:02 +0700

    update progress 2026-04-29 basic-day1

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print ("hello, world")
+# 2026-04-29 
+# Uji Coba Print Hello, World
+# print ("hello, world") 
+
+# Menggunakan variable
+var1 = 'Rhesa Prawedha'
+print ('my name is', var1)
+
+# Menggunakan f-strings
+var = "Dhantel Rhesa Prawedha"
+print(f"Hi my name {var}")
+
