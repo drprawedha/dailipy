fb050a6602a201a0b9cda1c31dd5fe516433deb4
Rhesa Prawedha <dr.prawedha@gmail.com>
2026-04-30 13:25:19 +0700

    update sample syntax

---
+++ syntax.py
@@ -0,0 +1,23 @@
+if 5 > 2:
+    print("five is greater than two")
+
+# best practices, buat setiap command di tiap line supaya enak dibaca
+
+
+# string
+# biasanya digunakan jika angka dianggap sebagai teks cth. format tertentu
+x = str(3)
+print('string')
+print (x)
+
+# integer
+# bilangan bulat / tidak butuh pecahan 
+y = int(3)
+print('integer')
+print (y)
+
+#float  
+# bilangan yang tidak bulat, koma, ada desimal
+z = float(3)
+print(float)
+print (z)
