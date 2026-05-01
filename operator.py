008091a58de05988ebb213077f5f3e7dbca989a9
Rhesa Prawedha <dr.prawedha@gmail.com>
2026-05-02 01:21:53 +0700

    sample operator aritmatika

---
+++ operator.py
@@ -0,0 +1,18 @@
+# operator untuk operasi matematis
+print(10+6)
+
+sum1 = 10 + 20
+sum2 = sum1 + 30
+sum3 = sum2 - sum1 - 30
+
+print(sum3)
+
+
+# aritmatik
+# +	    Addition	    x + y	
+# -	    Subtraction	    x - y	
+# *	    Multiplication	x * y	
+# /	    Division	    x / y	
+# %	    Modulus	        x % y	
+# **	Exponentiation	x ** y	
+# //	Floor division	x // y
