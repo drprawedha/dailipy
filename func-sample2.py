54cef87d27cde568fe7d258d944ce76ca2cba122
Rhesa Prawedha <dr.prawedha@gmail.com>
2026-05-04 10:31:26 +0700

    sample app shop calc

--- func-sample2.py
+++ func-sample2.py
@@ -45,50 +45,60 @@ def calculate_change(total,paid):
 
 def print_receipt(items,paid):
     print("============================")
-    print('       struk belanja')
+    print('       STRUK BELANJA')
     print("============================")
 
     grand_total = 0
     for item in items:
         total = calculate_total(item['price'],item['qty'])
         grand_total += total
-        print(f"Barang  : {item['name']}")
-        print(f"Harga   : Rp {item['price']}")
+        print(f"Item  : {item['name']}")
+        print(f"Price   : Rp {item['price']}")
         print(f"Qty     : {item['qty']}")
-        print(f"Total   :   Rp {total}")
+        print(f"Total   : Rp {total}")
         print("============================")
 
     change = calculate_change(grand_total, paid)
-    print(f"Grand Total : {grand_total}")
-    print(f"Paid        : {paid}")
-    print(f"Change      : {change}")
+    print(f"Grand Total : Rp {grand_total}")
+    print(f"Paid        : Rp {paid}")
+    print(f"Change      : Rp {change}")
     print("=============================")
 
+    if change == -1:
+        print(f"Amount due : Rp {grand_total - paid}")
+    else:
+        print(f"Change     : Rp {change}")
+
 def add_item():
-    name = input("Nama barang : ")
-    price = int(input("Harga "))
-    qty   = int(input("Jumlah "))
+    name    = input("Item Name : ")
+    price   = int(input("Price :"))
+    qty     = int(input("Qty :"))
+    discount = float(input("Discount (0-1): "))
     return {
         "name": name, 
         "price": price, 
-        "qty":qty}
+        "qty":qty,
+        "discount":discount}
 
 # --- main program ---
 items = []
 
 while True:
-    print("\n1. Tambah transaksi")
-    print("2. Cetak struk & selesai")
-    choice = input("Pilihan: ")
+    print("\n1. Add Transaction")
+    print("2. Print Receipt ")
+    choice = input("Option: ")
 
     if choice == "1":
         items.append(add_item())
     elif choice == "2":
-        paid = int(input("Jumlah bayar: "))
+        # hitung dulu grand total
+        grand_total = sum(calculate_total(item['price'], item['qty'], item['discount']) for item in items)
+        print(f"\nGrand Total : Rp {grand_total}")
+        paid = int(input("Paid: "))
         print_receipt(items, paid)
         break
     else:
-        print("Pilihan tidak valid.")
+        print("Option is not valid.")
 
 # ================================
 #         STRUK BELANJA
